3f6243dbe0ff185e99e71a96092977ccac8d556c
krios2146 <krios2146@gmail.com>
2025-12-16 13:59:10 +0700

    chore: fixed paths in build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 cargo build --release
-copy ".\target\release\sorceries_incantations_keybinds.dll" "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game\mod_dll\sorceries-incantations-keybinds\"
+copy ".\target\release\spell_keybinds.dll" "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game\mod_dll\spell-keybinds\"
